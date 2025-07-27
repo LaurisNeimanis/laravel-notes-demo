ef07f94039dc02734d9c2c69d820d549d9538a0d
Lauris Neimanis <neimanis.lauris@gmail.com>
2025-07-27 19:32:44 +0300

    Add notes route to authenticated group

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('notes', 'notes')->name('notes');
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
